47c123c2c890c95d9c1fd2f60fa8d1c64cdf0935
P-cpu911 <phonght.23bi14358@usth.edu.vn>
2026-03-24 03:01:46 +0700

    Update filecpy.c

--- Prototype/filecpy.c
+++ Prototype/filecpy.c
@@ -3,27 +3,37 @@
 #include <unistd.h>
 
 int main(){
-	FILE* src = fopen("./testpic.png", "rb");
-	FILE* dest = fopen("./testpic2.png", "ab");
-	unsigned char buf[1024];
-	int eof = 0;
-	while(eof == 0){
-		fread(buf, 1, 1024, src);
-		if(feof(src)){
-			eof = 1;
-		}
-		else if(ferror(src)){
-			printf("File (src) error\n");
-			eof = 2;
-		}
-		else{
-			//Nothing here. Just a placeholder
-		}
-		fwrite(buf, 1, 1024, dest);
-		if(ferror(dest)){
-			printf("File (dest) error\n");
-			eof = 2;
-		}
-	}
-	return 0;
+    FILE* src = fopen("./testpic.png", "rb");
+    if (!src) {
+        perror("fopen src");
+        return 1;
+    }
+
+    FILE* dest = fopen("./testpic2.png", "wb"); 
+    if (!dest) {
+        perror("fopen dest");
+        fclose(src);
+        return 1;
+    }
+
+    unsigned char buf[1024];
+    size_t bytesRead;
+
+    while ((bytesRead = fread(buf, 1, sizeof(buf), src)) > 0) {
+        fwrite(buf, 1, bytesRead, dest); 
+    }
+
+    if (ferror(src)) {
+        printf("Read error\n");
+    }
+
+    if (ferror(dest)) {
+        printf("Write error\n");
+    }
+
+    fclose(src);
+    fclose(dest);
+
+    printf("File copied successfully!\n");
+    return 0;
 }
