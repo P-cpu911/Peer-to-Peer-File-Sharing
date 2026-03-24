970977d66e8625536366fa82b18bec84339a8727
P-cpu911 <phonght.23bi14358@usth.edu.vn>
2026-03-25 03:51:39 +0700

    Update index_server.c

--- Prototype/index_server.c
+++ Prototype/index_server.c
@@ -21,12 +21,11 @@ int main() {
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[1024] = {0};
+    char buffer[1024];
 
-    // Style: System Startup
     printf("\033[0;36m[SYS_INIT] P2P Index Server starting on Port %d...\n", PORT);
     
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) exit(EXIT_FAILURE);
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     address.sin_family = AF_INET;
@@ -34,22 +33,37 @@ int main() {
     address.sin_port = htons(PORT);
 
     bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    listen(server_fd, 3);
+    listen(server_fd, 5);
 
     printf("[SYS_READY] Waiting for peer connections...\033[0m\n\n");
 
     while(1) {
         new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+        memset(buffer, 0, 1024);
         read(new_socket, buffer, 1024);
 
-        // Logic for SEED (Registration)
         if (strncmp(buffer, "SEED", 4) == 0) {
             sscanf(buffer, "SEED %s %d", registry[file_count].filename, &registry[file_count].port);
             strcpy(registry[file_count].ip, inet_ntoa(address.sin_addr));
-            
             printf("\033[0;32m[LOG_DATA] SEED_SUCCESS: '%s' registered from %s:%d\033[0m\n", 
                    registry[file_count].filename, registry[file_count].ip, registry[file_count].port);
             file_count++;
+        } 
+        else if (strncmp(buffer, "SEARCH", 6) == 0) {
+            char search_file[50];
+            sscanf(buffer, "SEARCH %s", search_file);
+            printf("[LOG_QUERY] Searching for: %s\n", search_file);
+            
+            int found = 0;
+            for(int i=0; i<file_count; i++) {
+                if(strcmp(registry[i].filename, search_file) == 0) {
+                    sprintf(buffer, "FOUND %s %d", registry[i].ip, registry[i].port);
+                    send(new_socket, buffer, strlen(buffer), 0);
+                    found = 1;
+                    break;
+                }
+            }
+            if(!found) send(new_socket, "NOT_FOUND", 9, 0);
         }
         close(new_socket);
     }
