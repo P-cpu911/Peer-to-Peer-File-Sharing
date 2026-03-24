d073dcb5b784fea03e43e99eb3929da76f345a10
P-cpu911 <phonght.23bi14358@usth.edu.vn>
2026-03-25 03:51:59 +0700

    Update peer.c

--- Prototype/peer.c
+++ Prototype/peer.c
@@ -5,11 +5,11 @@
 #include <pthread.h>
 #include <arpa/inet.h>
 
-#define INDEX_SERVER_PORT 8784
-#define MY_PORT 9000 // Change this for different peers (9001, 9002, etc.)
+#define INDEX_PORT 8784
+int MY_P2P_PORT; // Port for other peers to connect to
 
-// Server Thread: Listens for other peers wanting to download
-void *start_seeding(void *arg) {
+// Background thread: Listen for other peers to download from YOU
+void *seeder_thread(void *arg) {
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
@@ -17,51 +17,77 @@ void *start_seeding(void *arg) {
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(MY_PORT);
+    address.sin_port = htons(MY_P2P_PORT);
 
     bind(server_fd, (struct sockaddr *)&address, sizeof(address));
     listen(server_fd, 5);
 
     while(1) {
         new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-        printf("\n\033[0;35m[THREAD_02] Incoming download request! Sending data...\033[0m\n");
-        send(new_socket, "FILE_DATA_STREAM_SUCCESS", 24, 0);
+        char content[] = "This is the content of the file you requested.";
+        send(new_socket, content, strlen(content), 0);
         close(new_socket);
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc < 2) { printf("Usage: ./peer <port>\n"); return 1; }
+    MY_P2P_PORT = atoi(argv[1]);
+
     pthread_t tid;
-    pthread_create(&tid, NULL, start_seeding, NULL);
+    pthread_create(&tid, NULL, seeder_thread, NULL);
 
-    printf("\033[0;36m[root@RHEL10 ~]# P2P NODE INITIALIZED\n");
-    printf("PORT: %d | STATUS: SEEDING_ACTIVE\033[0m\n\n", MY_PORT);
+    printf("\033[0;34m[root@RHEL10 ~]# P2P NODE INITIALIZED\n");
+    printf("PORT: %d | STATUS: SEEDING_ACTIVE\033[0m\n\n", MY_P2P_PORT);
 
-    char command[100], filename[50];
-    
+    char cmd[20], file[50], buffer[1024];
+
     while(1) {
-        printf("Enter command (SEED <filename> / exit): ");
-        scanf("%s", command);
+        printf("Enter command (SEED <filename> / SEARCH <filename> / GET <filename> / exit): ");
+        scanf("%s", cmd);
 
-        if (strcmp(command, "SEED") == 0) {
-            scanf("%s", filename);
-            int sock = socket(AF_INET, SOCK_STREAM, 0);
-            struct sockaddr_in serv_addr;
-            serv_addr.sin_family = AF_INET;
-            serv_addr.sin_port = htons(INDEX_SERVER_PORT);
-            inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
-
-            if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == 0) {
-                char msg[100];
-                sprintf(msg, "SEED %s %d", filename, MY_PORT);
-                send(sock, msg, strlen(msg), 0);
+        if (strcmp(cmd, "SEED") == 0) {
+            scanf("%s", file);
+            int s = socket(AF_INET, SOCK_STREAM, 0);
+            struct sockaddr_in addr = {AF_INET, htons(INDEX_PORT)};
+            inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+            if(connect(s, (struct sockaddr *)&addr, sizeof(addr)) == 0) {
+                sprintf(buffer, "SEED %s %d", file, MY_P2P_PORT);
+                send(s, buffer, strlen(buffer), 0);
                 printf("\033[0;32m[SUCCESS] File registered on Index Server.\033[0m\n");
             }
-            close(sock);
-        } else if (strcmp(command, "exit") == 0) {
-            printf("\033[0;31m[root@RHEL10 ~]# exit\nLogout... System Offline.\033[0m\n");
-            exit(0);
+            close(s);
+        } 
+        else if (strcmp(cmd, "SEARCH") == 0) {
+            scanf("%s", file);
+            int s = socket(AF_INET, SOCK_STREAM, 0);
+            struct sockaddr_in addr = {AF_INET, htons(INDEX_PORT)};
+            inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+            connect(s, (struct sockaddr *)&addr, sizeof(addr));
+            sprintf(buffer, "SEARCH %s", file);
+            send(s, buffer, strlen(buffer), 0);
+            memset(buffer, 0, 1024);
+            recv(s, buffer, 1024, 0);
+            printf("\033[0;33m[INDEX_RESULT] %s\033[0m\n", buffer);
+            close(s);
         }
+        else if (strcmp(cmd, "GET") == 0) {
+            scanf("%s", file);
+            char target_ip[20]; int target_port;
+            printf("Enter Peer IP and Port from SEARCH result: ");
+            scanf("%s %d", target_ip, &target_port);
+
+            int s = socket(AF_INET, SOCK_STREAM, 0);
+            struct sockaddr_in addr = {AF_INET, htons(target_port)};
+            inet_pton(AF_INET, target_ip, &addr.sin_addr);
+            if(connect(s, (struct sockaddr *)&addr, sizeof(addr)) == 0) {
+                memset(buffer, 0, 1024);
+                recv(s, buffer, 1024, 0);
+                printf("\033[1;32m[DOWNLOAD_COMPLETE] Content: %s\033[0m\n", buffer);
+            }
+            close(s);
+        }
+        else if (strcmp(cmd, "exit") == 0) { printf("Shutting down...\n"); exit(0); }
     }
     return 0;
 }
