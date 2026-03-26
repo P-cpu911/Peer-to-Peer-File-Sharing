846eb19fa756f8b05ee054e2ec1a20ec8c5d6b4f
P-cpu911 <phonght.23bi14358@usth.edu.vn>
2026-03-27 04:19:07 +0700

    Update peer.c

--- peer.c
+++ peer.c
@@ -30,17 +30,20 @@ void send_file(int socket, const char *filename) {
         send(socket, msg, strlen(msg), 0);
         return;
     }
+
     char buffer[1024];
     size_t n;
     while ((n = fread(buffer, 1, sizeof(buffer), fp)) > 0) {
         send(socket, buffer, n, 0);
     }
+
     fclose(fp);
 }
 
 void *server_thread(void *arg) {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in address;
+
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(MY_P2P_PORT);
@@ -48,6 +51,7 @@ void *server_thread(void *arg) {
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("bind"); exit(1);
     }
+
     listen(server_fd, BACKLOG);
 
     while (1) {
@@ -56,9 +60,11 @@ void *server_thread(void *arg) {
 
         char request[256];
         int n = recv(new_socket, request, sizeof(request)-1, 0);
+
         if (n > 0) {
             request[n] = '\0';
             request[strcspn(request, "\r\n")] = '\0';
+
             if (is_seeded(request)) {
                 send_file(new_socket, request);
             } else {
@@ -66,6 +72,7 @@ void *server_thread(void *arg) {
                 send(new_socket, msg, strlen(msg), 0);
             }
         }
+
         close(new_socket);
     }
     return NULL;
@@ -98,6 +105,7 @@ void register_file(const char *file) {
     } else {
         perror("[ERROR] Connect to Index Server failed");
     }
+
     close(s);
 }
 
@@ -105,14 +113,18 @@ void get_my_ip() {
     int s = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr = {AF_INET, htons(INDEX_PORT)};
     inet_pton(AF_INET, INDEX_IP, &addr.sin_addr);
+
     if (connect(s, (struct sockaddr *)&addr, sizeof(addr)) == 0) {
         send(s, "HELLO", 5, 0);
-        char buf[100]; int n = recv(s, buf, sizeof(buf)-1, 0);
+        char buf[100];
+        int n = recv(s, buf, sizeof(buf)-1, 0);
+
         if (n > 0) {
             buf[n] = '\0';
             sscanf(buf, "YOUR_IP %s", my_ip);
         }
     }
+
     close(s);
 }
 
@@ -121,23 +133,21 @@ int main(int argc, char *argv[]) {
         printf("Usage: %s [Index_server_IP] [Index_server_port] [Peer_server_port]\n", argv[0]);
         return 1;
     }
+
     strncpy(INDEX_IP, argv[1], sizeof(INDEX_IP)-1);
     INDEX_PORT = atoi(argv[2]);
     MY_P2P_PORT = atoi(argv[3]);
 
-    // Get our IP from Index Server
     get_my_ip();
 
-    // Print startup banner
     printf("[SERVER] P2P NODE INITIALIZED\n");
     printf("IP: %s | PORT: %d | STATUS: SEEDING_ACTIVE\n\n", my_ip, MY_P2P_PORT);
 
-    // Start server thread
     pthread_t tid;
     pthread_create(&tid, NULL, server_thread, NULL);
 
-    // --- CLIENT INTERFACE ---
     char cmd[20], file[256], buffer[1024];
+
     while (1) {
         printf("Enter command (SEARCH <filename> / GET <filename> / SEED <filename> / exit): ");
         fflush(stdout);
@@ -146,63 +156,103 @@ int main(int argc, char *argv[]) {
 
         if (strcasecmp(cmd, "SEARCH") == 0) {
             if (scanf("%255s", file) != 1) continue;
+
             int s = socket(AF_INET, SOCK_STREAM, 0);
             struct sockaddr_in addr = {AF_INET, htons(INDEX_PORT)};
             inet_pton(AF_INET, INDEX_IP, &addr.sin_addr);
+
             if (connect(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
                 perror("[ERROR] Connect to Index Server failed");
-                close(s); continue;
+                close(s);
+                continue;
             }
+
             sprintf(buffer, "SEARCH %s", file);
             send(s, buffer, strlen(buffer), 0);
+
             memset(buffer, 0, 1024);
             int n = recv(s, buffer, 1024, 0);
+
             if (n > 0) {
                 buffer[n] = '\0';
                 printf("[INDEX_RESULT]\n%s\n", buffer);
             }
+
             close(s);
         }
+
         else if (strcasecmp(cmd, "GET") == 0) {
             if (scanf("%255s", file) != 1) continue;
-            char target_ip[20]; int target_port;
+
+            char target_ip[20];
+            int target_port;
+
             printf("Enter Peer IP and Port from SEARCH result: ");
             fflush(stdout);
+
             if (scanf("%19s %d", target_ip, &target_port) != 2) continue;
 
             int s = socket(AF_INET, SOCK_STREAM, 0);
             struct sockaddr_in addr = {AF_INET, htons(target_port)};
             inet_pton(AF_INET, target_ip, &addr.sin_addr);
+
+            printf("[CONNECTING TO %s:%d]\n", target_ip, target_port);
+
             if (connect(s, (struct sockaddr *)&addr, sizeof(addr)) == 0) {
-                send(s, file, strlen(file), 0);
+
+                char req[300];
+                sprintf(req, "%s\n", file);
+                send(s, req, strlen(req), 0);
+
                 FILE *fp = fopen(file, "wb");
-                if (!fp) { printf("[ERROR] Could not create local file.\n"); close(s); continue; }
+                if (!fp) {
+                    printf("[ERROR] Could not create local file.\n");
+                    close(s);
+                    continue;
+                }
+
                 int n, total = 0;
-                while ((n = recv(s, buffer, sizeof(buffer)-1, 0)) > 0) {
-                    buffer[n] = '\0';
+
+                while ((n = recv(s, buffer, sizeof(buffer), 0)) > 0) {
                     if (total == 0 && strncmp(buffer, "[ERROR]", 7) == 0) {
-                        printf("%s\n", buffer);
-                        fclose(fp); remove(file); break;
+                        printf("%.*s\n", n, buffer);
+                        fclose(fp);
+                        remove(file);
+                        break;
                     }
+
                     fwrite(buffer, 1, n, fp);
                     total += n;
+
+                    printf("[RECEIVING %d bytes]\n", n);
                 }
-                if (total > 0) printf("[DOWNLOAD_COMPLETE] Saved to %s\n", file);
+
+                if (total > 0)
+                    printf("[DOWNLOAD_COMPLETE] Saved to %s\n", file);
+
                 fclose(fp);
-            } else perror("[ERROR] Connect to peer failed");
+
+            } else {
+                perror("[ERROR] Connect to peer failed");
+            }
+
             close(s);
         }
+
         else if (strcasecmp(cmd, "SEED") == 0) {
             if (scanf("%255s", file) != 1) continue;
             register_file(file);
         }
+
         else if (strcasecmp(cmd, "exit") == 0) {
             printf("Shutting down peer...\n");
             exit(0);
         }
+
         else {
-            printf("[ERROR] Unknown command. Use SEARCH, GET, SEED, or exit.\n");
+            printf("[ERROR] Unknown command.\n");
         }
     }
+
     return 0;
 }
